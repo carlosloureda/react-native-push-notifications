66a2eb65626159c37a70d80a95470c4cc5bdbed1
Carlos Loureda <balanzeneto@gmail.com>
2019-06-26 14:12:00 +0200

    Added web for sending messages

--- packages/server/index.js
+++ packages/server/index.js
@@ -1,21 +1,19 @@
 const express = require("express");
 import { saveToken, sendMessage } from "./expo-server";
-
+var cors = require("cors");
 var bodyParser = require("body-parser");
 var app = express();
 
+app.use(cors()); //TODO: for production not the best idea
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
+// Then use it before your routes are set up:
 
 app.get("/", function(req, res) {
   res.send("Hello World!");
-});
-
-app.listen(3000, function() {
-  console.log("Example app listening on port 3000!");
 });
 
 app.post("/token", (req, res) => {
@@ -28,4 +26,7 @@ app.post("/message", (req, res) => {
   sendMessage(req.body.message);
   console.log(`Received message, ${req.body.message}`);
   res.send(`Received message, ${req.body.message}`);
+});
+app.listen(3000, function() {
+  console.log("Example app listening on port 3000!");
 });
---
+++ packages/web/src/App.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./App.css";
+const MESSAGE_ENPOINT = "http://49c4add7.ngrok.io/message";
+
+export default class App extends React.Component {
+  state = {
+    message: ""
+  };
+
+  sendMessage = e => {
+    e.preventDefault();
+    fetch(MESSAGE_ENPOINT, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        crossOrigin: false
+      },
+      body: JSON.stringify({ message: this.state.message })
+    }).catch(e => console.warn("error fetching: ", e));
+  };
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Send a notification to the mobile device:</h1>
+          <form>
+            <input
+              type="text"
+              value={this.state.message}
+              onChange={e => this.setState({ message: e.target.value })}
+            />
+
+            <button onClick={e => this.sendMessage(e)}>Send</button>
+          </form>
+        </header>
+      </div>
+    );
+  }
+}
